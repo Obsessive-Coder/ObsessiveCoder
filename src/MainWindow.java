788b84ef7da2b258c102ece597de7dd261872f1b
jared huffstutler <jaredhuff85@gmail.com>
2019-02-13 21:48:55 -0600

    refactored MainWindow class

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -25,34 +25,10 @@ public class MainWindow {
 		this.frame.setSize(new Dimension(400, 400));
 		this.frame.setLocationRelativeTo(null);
 		
-		this.frame.getContentPane().add(this.welcomeText, BorderLayout.NORTH);
+		this.frame.add(this.welcomeText, BorderLayout.NORTH);
 		
-		JPanel needsPanel = new JPanel();
-		JPanel statsPanel = new JPanel();
-		needsPanel.setLayout(new BoxLayout(needsPanel, BoxLayout.PAGE_AXIS));
-		statsPanel.setLayout(new BoxLayout(statsPanel, BoxLayout.PAGE_AXIS));
-		needsPanel.setMinimumSize(new Dimension(200, 200));
-		statsPanel.setMinimumSize(new Dimension(200, 200));
-		
-		JLabel needsText = new JLabel("Needs", JLabel.CENTER);
-		needsText.setMinimumSize(new Dimension(200, 25));
-		needsText.setPreferredSize(new Dimension(200, 25));
-		needsText.setMaximumSize(new Dimension(200, 25));
-		needsPanel.add(needsText, JLabel.CENTER);
-		Map<String, Integer> needs = this.coder.getNeeds();
-		for(String need : needs.keySet()) {
-			needsPanel.add(new JLabel(need + ": " + needs.get(need)));
-		}
-		
-		JLabel statsText = new JLabel("Stats", JLabel.CENTER);
-		statsText.setMinimumSize(new Dimension(200, 25));
-		statsText.setPreferredSize(new Dimension(200, 25));
-		statsText.setMaximumSize(new Dimension(200, 25));
-		statsPanel.add(statsText, JLabel.CENTER);
-		Map<String, Integer> statistics = this.coder.getStatistics();
-		for(String stat : statistics.keySet()) {
-			statsPanel.add(new JLabel(stat + ": " + statistics.get(stat)));
-		}
+		JPanel needsPanel = this.buildInfoPanel("Needs", this.coder.getNeeds());
+		JPanel statsPanel = this.buildInfoPanel("Statistics", this.coder.getStatistics());
 		
 		this.infoPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, needsPanel, statsPanel);
 		this.infoPane.setDividerSize(0);
@@ -61,5 +37,24 @@ public class MainWindow {
 		this.frame.add(this.infoPane);
 		
 		this.frame.setVisible(true);
+	}
+	
+	private JPanel buildInfoPanel(String type, Map<String, Integer> info) {		
+		JLabel infoTitle = new JLabel(type, JLabel.CENTER);
+		infoTitle.setMinimumSize(new Dimension(200, 25));
+		infoTitle.setPreferredSize(new Dimension(200, 25));
+		infoTitle.setMaximumSize(new Dimension(200, 25));
+		
+		JPanel infoPanel = new JPanel();
+		infoPanel.setLayout(new BoxLayout(infoPanel, BoxLayout.Y_AXIS));
+		infoPanel.setMinimumSize(new Dimension(200, 200));
+		
+		infoPanel.add(infoTitle);
+		
+		for(String key : info.keySet()) {
+			infoPanel.add(new JLabel(key + ": " + info.get(key)));
+		}
+		
+		return infoPanel;
 	}
 }
